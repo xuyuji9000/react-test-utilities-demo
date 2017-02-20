3c32f60d1c527f7907a5e590d97e57a26e45bfee
Karl Xu <karl.yogi.xu@gmail.com>
2017-02-20 17:56:10 +0800

    add MyComponent test

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactTestUtils from "react-addons-test-utils";
+import expect from "expect";
+
 import MyComponent from "./MyComponent";
+import SubComponent from "./SubComponent";
 
-const renderer = ReactTestUtils.createRenderer();
-renderer.render(<MyComponent></MyComponent>);
-const result = renderer.getRenderOutput();
+describe("MyComponent",function(){
 
-expect(result.type).toBe("div");
+
+    it("is a div", function() {
+        const renderer = ReactTestUtils.createRenderer();
+        renderer.render(<MyComponent></MyComponent>);
+        const result = renderer.getRenderOutput();
+
+        expect(result.type).toBe("div");
+    });
+
+    it("has specific content",function() {
+        const renderer = ReactTestUtils.createRenderer();
+        renderer.render(<MyComponent></MyComponent>);
+        const result = renderer.getRenderOutput();
+
+        expect(result.props.children).toEqual([
+            <span className="heading">Title</span>,
+            <SubComponent foo="bar"></SubComponent>
+        ]);
+    });
+});
 
